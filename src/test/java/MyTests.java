b1ce4415663bc6230bb5a12fe6e52fc94b3224a5
Amogh Rathore <a18996@PC-4816.local>
2016-07-29 18:28:53 +0900

    unit tests

---
+++ src/test/java/MyTests.java
@@ -0,0 +1,13 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+import hello.Arithmetic;
+
+public class MyTests {
+  @Test
+  public void multiplicationTest() {
+    Arithmetic tester = new Arithmetic();
+
+    assertEquals("3 x 15 must be 45", 45, tester.multiply(3, 15));
+    assertEquals("5 x 20 must be 100", 100, tester.multiply(5, 20));
+  }
+}
