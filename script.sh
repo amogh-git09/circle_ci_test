2078fb1393039ddc3444b3e3fbd108ce96c771de
Amogh Rathore <a18996@PC-4816.local>
2016-08-05 13:46:31 +0900

    docker-compose test

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 python getModules.py
 modules=$(awk "NR % ${CIRCLE_NODE_TOTAL} == ${CIRCLE_NODE_INDEX}" __module_list.txt)
-str="gradle "
+str="gradle --info "
 for module in $modules; do
   str+=":$module:test "
 done
