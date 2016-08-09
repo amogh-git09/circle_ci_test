002e6ea0461e5f3a0e3fa3cc8795a26678edf509
Amogh Rathore <a18996@PC-4816.local>
2016-08-09 15:42:17 +0900

    deployment

--- script.sh
+++ script.sh
@@ -1,11 +1,13 @@
-python getModules.py
-modules=$(awk "NR % ${CIRCLE_NODE_TOTAL} == ${CIRCLE_NODE_INDEX}" __module_list.txt)
-str="gradle --info "
-for module in $modules; do
-  str+=":$module:test "
-done
-$str
-
-# cd rails_module
-# bin/rails test
-# cd ..
+if [ "$CIRCLE_BRANCH" != "dev" ]
+then
+  python getModules.py
+  modules=$(awk "NR % ${CIRCLE_NODE_TOTAL} == ${CIRCLE_NODE_INDEX}" __module_list.txt)
+  str="gradle --info "
+  for module in $modules; do
+    str+=":$module:test "
+  done
+  $str
+  cd rails_module
+  bin/rails test
+  cd ..
+fi
