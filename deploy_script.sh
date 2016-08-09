32edbdcf04e66b7495ee7bc3bd9ec76075f4944e
Amogh Rathore <a18996@PC-4816.local>
2016-08-09 11:38:16 +0900

    deployment

--- deploy_script.sh
+++ deploy_script.sh
@@ -1,10 +1,13 @@
-modules=$(awk "NR % ${CIRCLE_NODE_TOTAL} == ${CIRCLE_NODE_INDEX}" __module_list.txt)
-str="gradle --info "
-for module in $modules; do
-  str+=":$module:build "
-done
-$str
+if [ "$CIRCLE_BRANCH" == "dev" ] || [ "$CIRCLE_BRANCH" == "stg" ] || [ "$CIRCLE_BRANCH" == "prod" ]
+then
+  modules=$(awk "NR % ${CIRCLE_NODE_TOTAL} == ${CIRCLE_NODE_INDEX}" __module_list.txt)
+  str="gradle --info "
+  for module in $modules; do
+    str+=":$module:build "
+  done
+  $str
 
-for module in $modules; do
-  aws s3 sync $module/build/libs s3://tenma-dev-dap/circle_ci/$module --delete
-done
+  for module in $modules; do
+    aws s3 sync $module/build/libs s3://tenma-dev-dap/circle_ci/$module --delete
+  done
+fi
