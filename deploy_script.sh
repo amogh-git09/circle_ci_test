3ae69017cff22961bf5a659d2b8f8ff0a219aa99
Amogh Rathore <a18996@PC-4816.local>
2016-08-09 11:18:54 +0900

    deployment

---
+++ deploy_script.sh
@@ -0,0 +1,10 @@
+modules=$(awk "NR % ${CIRCLE_NODE_TOTAL} == ${CIRCLE_NODE_INDEX}" __module_list.txt)
+str="gradle --info "
+for module in $modules; do
+  str+=":$module:build "
+done
+$str
+
+for module in $modules; do
+  aws s3 sync $module/build/libs s3://tenma-dev-dap/circle_ci/$module --delete
+done
