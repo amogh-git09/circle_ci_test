8e63cf06fa16e0422ad79e1c3e82ba379d1de951
Amogh Rathore <a18996@PC-4816.local>
2016-08-12 15:36:17 +0900

    circleci testing

--- mysql_module/src/main/java/dbtest/MySqlTest.java
+++ mysql_module/src/main/java/dbtest/MySqlTest.java
@@ -4,7 +4,7 @@ import java.sql.*;
 
 public class MySqlTest {
   static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
-  static final String DB_URL = "jdbc:mysql://localhost:5000";
+  static final String DB_URL = "jdbc:mysql://localhost:3306";
 
   static final String USER = "root";
   static final String PASS = "root";
